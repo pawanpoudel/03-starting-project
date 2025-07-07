f30caadbdc86094caf77afe58c1c4d304204a8c0
Pawan Poudel <pawan.poudel@gmail.com>
2025-07-07 15:33:31 -0400

    Add dummy comment to kick of workflow

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,8 @@ import db from '../data/database.js';
 
 const router = Router();
 
+// Some comment
+
 router.get('/', async (req, res) => {
   const allEvents = await db.collection('events').find().toArray();
   res.json({ events: allEvents });
@@ -11,7 +13,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const eventData = req.body;
-  const result = await db.collection('events').insertOne({...eventData});
+  const result = await db.collection('events').insertOne({ ...eventData });
   res.status(201).json({
     message: 'Event created.',
     event: { ...eventData, id: result.insertedId },
